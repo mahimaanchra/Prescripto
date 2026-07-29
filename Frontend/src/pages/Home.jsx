d2c4ce287055f24a34ec63ef9ccd2dda66a20bff
mahimaanchra <mahimaanchra@gmail.com>
2026-07-29 22:49:56 +0530

    Speciality Menu

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import NavBar from '../components/NavBar'
+import SpecialityMenu from '../components/SpecialityMenu'
 
 const Home = () => {
   return (
     <div>
       <NavBar/>
       <Header/>
+      <SpecialityMenu/>
     </div>
   )
 }
