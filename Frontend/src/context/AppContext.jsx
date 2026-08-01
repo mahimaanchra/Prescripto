7ecff2952d8d91370c22692787408fae8751e5ea
mahimaanchra <mahimaanchra@gmail.com>
2026-08-01 23:53:57 +0530

    Doctors profile

--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -3,8 +3,10 @@ import { doctors } from "../../assets 2/assets_frontend/assets";
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
+    const currency = '$';
+
     const value = {
-     doctors 
+     doctors , currency
     };
 
     return (
--- Frontend/src/pages/Appointment.jsx
+++ Frontend/src/pages/Appointment.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState , useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
+import { assets } from '../../assets 2/assets_frontend/assets';
 
 const Appointment = () => {
-  return (
+
+  const {docId} = useParams();
+  const {doctors , currency} = useContext(AppContext);
+  const [docInfo , setDocInfo] = useState(null);
+  const [docSlots , setDocSlots] = useState([]);
+  const [slotIndex , setSlotIndex] = useState(0);
+  const [slotTime , setSlotTime] = useState('');
+
+  const getAvailableSlot = async () => {
+     setDocSlots([])
+
+     let today = new Date();
+     for( let i = 0 ; i < 7 ; i++){
+      // get date with index
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate()+i);
+      // setting end time of the date with index
+      let endTime = new Date()
+      endTime.setDate(today.getDate()+1);
+      endTime.setHours(21,0,0)
+      //setting hours
+      //if(today.getDate() === currentDate.getDate())
+     }
+  }
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find(doc => doc._id === docId);
+    setDocInfo(docInfo);
+  }
+  useEffect(()=>{
+   fetchDocInfo();
+  },[doctors,docId])
+
+  useEffect(()=>{
+    getAvailableSlot();
+  },docInfo)
+  return docInfo && (
     <div>
-      
+     {/*-----------Doctor Details-------------*/} 
+     <div className='flex flex-col sm:flex-row gap-4'>
+      <div>
+        <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
+      </div>
+      <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:max-0 mt-[-80px] sm:mt-0'>
+        {/*-----------Doc Info : name , degree , experience-----------------*/}
+        <p className='flex iten-center gap-2 text-2xl font-medium text-gray-900'>
+          {docInfo.name}
+          <img src={assets.verified_icon} alt="" />
+           </p>
+         <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
+           <p>
+            {docInfo.degree} - {docInfo.speciality}
+          </p>
+          <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience}</button>
+         </div>
+         {/*-----------Doctor About------------------*/}
+         <div>
+          <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>About <img src={assets.info_icon} alt="" /></p>
+          <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
+         </div>
+         <p className='text-gray-500 font-medium mt-4'>Appointment fee: <span>{currency}{docInfo.fees}</span></p>
+      </div>
+     </div>
     </div>
   )
 }
