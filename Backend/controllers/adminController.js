121f9a2252fab993e1764845cc7957c04de7e35f
mahimaanchra <mahimaanchra@gmail.com>
2026-08-16 14:27:19 +0530

    add doctors

---
+++ Backend/controllers/adminController.js
@@ -0,0 +1,10 @@
+const addDoctor = async () => {
+    try {
+       const {name , email , password , speciality , degree , experience , about , fees , address} = request.body;
+
+    } catch (error) {
+        
+    }
+}
+
+export {addDoctor}
