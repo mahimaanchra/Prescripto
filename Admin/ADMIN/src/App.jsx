8ce7f5f787a6d75c2272feebefe0a936c0d7b2cc
mahimaanchra <mahimaanchra@gmail.com>
2026-08-25 14:18:09 +0530

    Login page

--- Admin/ADMIN/src/App.jsx
+++ Admin/ADMIN/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify';
+import { AdminContext } from './context/AdminContext';
+  
 
 const App = () => {
-  return (
-    <div>
+
+  const {aToken} = useContext(AdminContext)
+
+  return aToken ? (
+  <div>
+    <ToastContainer/>
+  </div>
+  )
+  : (
+   <>
       <Login/>
-    </div>
+      <ToastContainer/>
+   </>
   )
 }
 
