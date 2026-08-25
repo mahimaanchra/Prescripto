c3b6f1d873afbfc194dbd4c3f26c872675aad2c8
mahimaanchra <mahimaanchra@gmail.com>
2026-08-25 14:30:22 +0530

    Navbar

--- Admin/ADMIN/src/App.jsx
+++ Admin/ADMIN/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
   
 
 const App = () => {
@@ -9,8 +10,9 @@ const App = () => {
   const {aToken} = useContext(AdminContext)
 
   return aToken ? (
-  <div>
+  <div className='bg-[#F8F9FD]'>
     <ToastContainer/>
+    <Navbar/>
   </div>
   )
   : (
