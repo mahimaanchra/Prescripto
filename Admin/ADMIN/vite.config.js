7cf489a65caf6d516e5f6f3c47c946555f5bc036
mahimaanchra <mahimaanchra@gmail.com>
2026-09-07 23:50:46 +0530

    restored accidently deleted code

--- Admin/ADMIN/vite.config.js
+++ Admin/ADMIN/vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from 'vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // 👈 Add Tailwind v4 plugin here
-  ],
-  resolve: {
-    dedupe: ['react', 'react-dom'],
-    alias: {
-      'react': path.resolve(import.meta.dirname, './node_modules/react'),
-      'react-dom': path.resolve(import.meta.dirname, './node_modules/react-dom'),
-    },
-  },
+  plugins: [react()],
 })
